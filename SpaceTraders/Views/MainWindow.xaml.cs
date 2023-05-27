991f5780881073e3001749aba450f16df8aefd71
Kevin Borka <github@borka.io>
2023-05-27 00:37:27 -0500

    Windows Title Bar Theming
    Added interoperable code to give window title bar the dark theme.

--- SpaceTraders/Views/MainWindow.xaml.cs
+++ SpaceTraders/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace SpaceTraders.Views
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace SpaceTraders.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +12,34 @@
         public MainWindow()
         {
             InitializeComponent();
+            IntPtr hwnd = new WindowInteropHelper(this).EnsureHandle();
+            UserImmersiveDarkMode(hwnd, true);
         }
+
+        [DllImport("dwmapi.dll", PreserveSig = true)]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+        private const int DWMWA_USER_IMERSIVE_DARK_MODE = 20;
+
+        private static bool UserImmersiveDarkMode(IntPtr handle, bool enabled)
+        {
+            if (!IsWindows10OrGreater(17763))
+            {
+                return false;
+            }
+
+            var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+            if (IsWindows10OrGreater(18985))
+            {
+                attribute = DWMWA_USER_IMERSIVE_DARK_MODE;
+            }
+
+            int useImmersiveDarkMode = enabled ? 1 : 0;
+            return DwmSetWindowAttribute(handle, (int)attribute, ref useImmersiveDarkMode, sizeof(int)) == 0;
+        }
+
+        private static bool IsWindows10OrGreater(int build = -1) => Environment.OSVersion.Version.Major >= 10 &&
+                                                                    Environment.OSVersion.Version.Build >= build;
     }
 }
